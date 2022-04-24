fb9811d13bb93ab56212aabd9ef7fd2d328183f5
vagrant <michelina.martinez@gmail.com>
2022-04-24 20:49:43 +0000

    for and while loops iterating over an array

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//for loops are a good option for iterating over items in an array
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
 	console.log(packingList[i]);
+}
+
+//while loops can also produce the same result - it separates the logic into a few more lines though
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+	console.log(packingList[i]);
+	i++;
 }
