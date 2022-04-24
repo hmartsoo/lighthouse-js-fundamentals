082a5e2fa139d97c124f98e3d72f61449d7d4e56
vagrant <michelina.martinez@gmail.com>
2022-04-24 20:04:32 +0000

    JS arrays intro exercise with ice cream flavours

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//Add the flavour "root beer" to the array without modifying the original array. Then console.log the value of the entire array.
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//console.log the first flavour in the array.
+console.log(iceCreamFlavours[0]);
+
+//console.log the last flavour in the array
+console.log(iceCreamFlavours[5]);
+
+//console.log the resulting number of flavours in the array
+console.log(iceCreamFlavours.length);
